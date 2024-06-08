e069450af4b4018303b2c7f830c2b45e3f29ee22
Nishant Guvvada <nishant.guvvada@gmail.com>
2024-06-08 19:15:13 +0530

    First Ethereum Project

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xa36EfF8a77E804678fC0BD3A1b89009F20C0360B',
+    '1000000000000000000000'
+  )
+};
